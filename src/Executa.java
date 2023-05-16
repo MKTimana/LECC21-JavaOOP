7b1d86ed5a4e1bd428e855caa0a9c3c8d7a6eae5
Milton Timana <miltonkevin10@gmail.com>
2023-05-16 13:12:28 +0200

    Update vector

--- src/Executa.java
+++ src/Executa.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Executa {
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
-        Vector v = new Vector();
+        Vector v = new Vector(3);
         Operacoes op = new Operacoes();
         int id;
-        for(int i = 0; i < 4; i++) {
+        for(int i = 0; i < v.capacity(); i++) {
             id = i;
 
             System.out.println("Insira o nome do Filme");
@@ -25,5 +25,22 @@ public class Executa {
         }
 
         op.index(v);
+
+        System.out.println("Partindo para a atualização...");
+        System.out.print("Insira o genero que deseja atualizar...: ");
+        String genero = input.nextLine();
+        System.out.print("Insira o novo genero: ");
+        String ng = input.nextLine();
+
+        op.update(ng,genero,v);
+//        for(int i = 0; i< v.capacity(); i++){
+//            if( ( (Filmes) v.get(i) ).getGenero().equalsIgnoreCase(genero) ){
+//                System.out.print("Para que genero deseja atualizar? ");
+//                String ng = input.nextLine();
+//
+//                ((Filmes) v.get(i)).setGenero(ng);
+//                System.out.print("Para que genero deseja atualizar? ");
+//            }
+//        }
     }
 }
