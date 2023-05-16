13503b29a8c3bd119c3cbb082128af87be09c381
Milton Timana <miltonkevin10@gmail.com>
2023-05-16 13:52:57 +0200

    CRUD working

--- src/Executa.java
+++ src/Executa.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Executa {
     public static void main(String[]args){
         Scanner input = new Scanner(System.in);
-        Vector v = new Vector(3);
+        Vector v = new Vector(2);
         Operacoes op = new Operacoes();
         int id;
         for(int i = 0; i < v.capacity(); i++) {
@@ -24,15 +24,17 @@ public class Executa {
             input.nextLine();
         }
 
+//        op.index(v);
+//
+//        System.out.println("Partindo para a atualização...");
+//        System.out.print("Insira o genero que deseja atualizar...: ");
+//        String genero = input.nextLine();
+//        System.out.print("Insira o novo genero: ");
+//        String ng = input.nextLine();
+//
+//        op.update(ng,genero,v);
         op.index(v);
-
-        System.out.println("Partindo para a atualização...");
-        System.out.print("Insira o genero que deseja atualizar...: ");
-        String genero = input.nextLine();
-        System.out.print("Insira o novo genero: ");
-        String ng = input.nextLine();
-
-        op.update(ng,genero,v);
+        //Isso foi adicionado na classe Operacoes!
 //        for(int i = 0; i< v.capacity(); i++){
 //            if( ( (Filmes) v.get(i) ).getGenero().equalsIgnoreCase(genero) ){
 //                System.out.print("Para que genero deseja atualizar? ");
@@ -42,5 +44,12 @@ public class Executa {
 //                System.out.print("Para que genero deseja atualizar? ");
 //            }
 //        }
+
+        System.out.println("Da lista de filmes, indique o numero do filme que deseja remover...");
+        int numero = input.nextInt();
+        numero = numero -1;
+        op.remove(v,numero);
+
+        op.index(v);
     }
 }
--- src/Operacoes.java
+++ src/Operacoes.java
@@ -11,17 +11,19 @@ public class Operacoes {
         for(int i = 0; i<v.size(); i++){
           if( ( (Filmes) v.get(i) ).getGenero().equalsIgnoreCase(ag)){
               ((Filmes) v.get(i)).setGenero(ng);
+              System.out.println("Genero do filme "+((Filmes) v.get(i)).getNome()+" actualizado com sucesso!");
           }
         }
     }
 
     public void index(Vector v){
         for(int i = 0; i < v.size(); i++){
-            System.out.println((Filmes) v.get(i));
+            System.out.println((i+1)+" "+ (Filmes) v.get(i));
         }
     }
 
     public void remove(Vector v, int i){
+        System.out.println("O filme "+((Filmes) v.get(i)).getNome()+" foi removido com sucesso!");
         v.removeElementAt(i);
     }
 }
